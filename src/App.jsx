5eefef42e4ef2c72b4b2086ba709cdb2b2d0cded
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2024-07-12 10:49:40 +0100

    App Handling Route

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,27 @@ import Header from "./Components/Header.jsx";
 import AddJuryMemberForm from "./Components/AddJuryMemberForm.jsx";
 import UpdateJuryMemberForm from "./Components/UpdateJuryMemberForm.jsx";
 import Sidebar from "./Components/Sidebar.jsx";
+import MeetingListPage from "./Components/MeetingListPage.jsx";
+import ScheduleMeeting from "./Components/ScheduleMeeting.jsx";
 
 function App() {
     return (
         <Router>
-            <div>
-                <Header />
-                {/*<Sidebar/>*/}
-                <Routes>
-                    <Route path="/Home" element={<JuryList />} />
-                    <Route path="/add-jury" element={<AddJuryMemberForm />} />
-                    <Route path="/Update/:id" element={<UpdateJuryMemberForm />} />
-                </Routes>
+            <div className="flex h-screen">
+                <Sidebar/>
+                <div className="flex-1 flex flex-col">
+                    <Header />
+                    <div className='p-10'>
+                        <Routes>
+                            <Route path="/Home" element={<JuryList />} />
+                            <Route path="/add-jury" element={<AddJuryMemberForm />} />
+                            <Route path="/Update/:id" element={<UpdateJuryMemberForm />} />
+                            <Route path="/MeetingListPage" element={<MeetingListPage />} />
+                            <Route path="/ScheduleMeeting" element={<ScheduleMeeting />} />
+                        </Routes>
+                    </div>
+                </div>
+
             </div>
         </Router>
     );
